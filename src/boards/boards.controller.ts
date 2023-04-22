a330d3746d975e337c111835d4013768bdcbad2f
clock509 <8kwchoi8@naver.com>
2023-04-22 23:10:02 +0900

    board 생성 DTO

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,7 @@
-import {Body, Controller, Get, Post} from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BoardsService} from './boards.service';
-import {Board} from "./boards.model";
+import { Board } from "./boards.model";
+import { CreateBoardDto } from "./dto/create-board.dto";
 
 @Controller('boards')
 export class BoardsController {
@@ -14,12 +15,14 @@ export class BoardsController {
   @Post()
   createBoard(
     // @Body() body, // 모든 body 파라미터에 접근이 가능
-    @Body('title') title: string,
-    @Body('description') description: string,
+    // @Body('title') title: string,
+    // @Body('description') description: string,
+    @Body() createBoardDto: CreateBoardDto
   ): Board {
     return this.boardsService.createBoard(
-      title,
-      description
+      // title,
+      // description
+      createBoardDto
     )
   }
 }
